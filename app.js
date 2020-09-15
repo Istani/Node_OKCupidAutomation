4ee082fd085dde8b394d9cbc2bab8a9c0547809c
Sascha Kaufmann <sascha.u.kaufmann@googlemail.com>
2020-09-15 11:02:37 +0200

    Running Minimal

--- app.js
+++ app.js
@@ -16,11 +16,6 @@ if (fs.existsSync(".env")) {
 }
 
 var queue = require("better-queue");
-var BumAPI = require("bumbleapi")(
-  process.env.BUMBLE_LOGIN,
-  process.env.BUMBLE_PASS
-);
-
 var q = new queue(
   function(input, cb) {
     input(cb);
@@ -28,82 +23,4 @@ var q = new queue(
   { afterProcessDelay: 1000 * 60 }
 );
 
-var count = 100;
-q.push(cb => {
-  Login(cb);
-});
-function NeustartUndSo(Zeit = 5) {
-  console.log(new Date(), "Restart in " + Zeit + " Min!");
-  setTimeout(() => {
-    count = 100;
-    main();
-  }, 1000 * 60 * Zeit);
-}
 
-
-async function Login(cb) {
-  BumAPI.startup().then(res => {
-    console.log(res);
-    q.push(cb => {
-      GetUser(cb);
-    });
-    cb();
-  });
-}
-async function GetUser(cb) {
-  try {
-    BumAPI.getEncounters().then(data => {
-      for (let i = 0; i < data.results.length; i++) {
-        if (count > 0) {
-          count--;
-          const element = data.results[i].user;
-          console.log(element);
-          save_file("P_" + element.user_id, element);
-          q.push(cb => {
-            Vote(element, cb);
-          });
-        } else {
-          NeustartUndSo(60*8);
-          cb();
-          return;
-        }
-      }
-
-      q.push(cb => {
-        GetUser(cb);
-      });
-      cb();
-    });
-  } catch (e) {
-    console.error(e);
-    process.exit(1);
-  }
-}
-async function Vote(user, cb) {
-  try {
-    BumAPI.voteYestForEncounter(user.user_id).then(data => {
-      console.log(data);
-      cb();
-    });
-  } catch (e) {
-    console.error(e);
-    process.exit(1);
-  }
-}
-async function save_file(name, data, only_new = false) {
-  try {
-    var filename = "./tmp/" + name + ".json";
-    var fs = require("fs");
-    if (fs.existsSync(filename)) {
-      if (only_new) {
-        return false;
-      }
-      fs.unlinkSync(filename);
-    }
-    fs.writeFileSync(filename, JSON.stringify(data, null, 2));
-    return true;
-  } catch (e) {
-    console.error(e);
-    return false;
-  }
-}
